bcb20161cbf415693828eb31c07f0749a8f6510f
KC Cushman <cushman.kc@gmail.com>
2024-01-05 12:10:55 -0500

    Update cloudCompareTemplate.bat
    Edit Cloud Compare command line parameters
    - remove "-ITER" so that default "-MIN_ERROR_DIFF" is used instead
    - increase overlap parameter to 98
    - increase "-RANDOM_SAMPLING_LIMIT" from default to 100,000

--- scripts/cloudCompareTemplate.bat
+++ scripts/cloudCompareTemplate.bat
@@ -2,7 +2,7 @@
 set loopcount=844
 :loop
 echo Iter %loopcount%
-CloudCompare -SILENT -AUTO_SAVE OFF -O -GLOBAL_SHIFT -620000.00 -1000000.00 0 "path\to\CURRENT\cloud\cloud_%LOOPCOUNT%.laz"  -O -GLOBAL_SHIFT -620000.00 -1000000.00 0  "path\to\PREVIOUS\cloud\BCI18r_%LOOPCOUNT%.laz" -ICP -ITER 800 -OVERLAP 80 -C_EXPORT_FMT LAS -SAVE_CLOUDS FILE  "path\to\OUTPUT\cloud\cloud_%LOOPCOUNT%.las <path\to\PREVIOUS\cloud\BCI18r_%LOOPCOUNT%.las" -CLEAR
+CloudCompare -SILENT -AUTO_SAVE OFF -O -GLOBAL_SHIFT -620000.00 -1010000.00 0 "path\to\CURRENT\cloud\cloud_%LOOPCOUNT%.laz"  -O -GLOBAL_SHIFT -620000.00 -1010000.00 0  "path\to\PREVIOUS\cloud\BCI18r_%LOOPCOUNT%.laz" -ICP -OVERLAP 98 -RANDOM_SAMPLING_LIMIT 100000 -C_EXPORT_FMT LAS -SAVE_CLOUDS FILE  "path\to\OUTPUT\cloud\cloud_%LOOPCOUNT%.las <path\to\PREVIOUS\cloud\BCI18r_%LOOPCOUNT%.las" -CLEAR
 set /a loopcount=loopcount-1
 if %loopcount%==0 goto exitloop
 goto loop
